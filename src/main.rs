55bceceb176742556b9a913d4148b26ebbcfd8f5
Lakin Wecker <lakin.wecker@gmail.com>
2019-12-08 10:10:51 -0700

    LICENSING it under the AGPLv3

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+// Limail an email helper for lichess
+// Copyright (C) 2019  Lakin Wecker
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 #![feature(proc_macro_hygiene, decl_macro)]
 #![feature(box_patterns)]
 
